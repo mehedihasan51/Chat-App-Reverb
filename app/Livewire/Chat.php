b7114cab3c28083040a5551a5d29272586889f2c
mehedihasan51 <mehediasan45@gmail.com>
2025-06-14 16:39:16 +0600

    U

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Chat extends Component
 {
+
+    public $users;
+    public $selectedUser = null;
+
+    public function mount()
+    {
+        if (Auth::check()) {
+            $this->users = User::where('id', '!=', Auth::id())->get();
+        } else {
+            $this->users = collect();
+        }
+    }
+
+    public function selectUser($userId)
+    {
+        $this->selectedUser = User::find($userId);
+    }
+
     public function render()
     {
         return view('livewire.chat');
