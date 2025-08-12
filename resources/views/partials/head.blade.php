470008f474caf9060fd47e00a3476127c02f3506
mehedihasan51 <mehediasan45@gmail.com>
2025-08-12 16:33:36 +0600

    U

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,8 +7,14 @@
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
+<!-- <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> -->
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+<link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
+@vite(['resources/js/app.js'])
+
+
+
+
+<!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
 @fluxAppearance
